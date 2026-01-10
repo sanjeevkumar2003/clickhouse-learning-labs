1ab2b006968f6835f6dd162aa8e62bedaeabbc5a
sanjeevkumar2003 <137679341+sanjeevkumar2003@users.noreply.github.com>
2026-01-10 18:06:47 +0530

    Refine comments and steps for Memory engine table
    Updated SQL comments and added steps for using the Memory engine table.

--- Task_09_Memory_engine_Table/queries.sql
+++ Task_09_Memory_engine_Table/queries.sql
@@ -1,19 +1,31 @@
-Task 9: Create a Memory Engine Table for Temporary Caching
-  
-Objective:
-  
-Use the in-memory storage engine to cache query results or hold transient data.
-  
-Steps:
-  
-Create a table using the Memory engine.
+-- Step 1:  Create a Table For memory engine
 
-Insert small sample data into it.
+CREATE TABLE IF NOT EXISTS engine_testing_dec23.memory_table
+(
+    id UInt16,
+    name String,
+    age UInt16
+)
+ENGINE = Memory;
 
-Verify that the data is immediately accessible for reads.
+-- Step 2 : Insert a data into table
 
-Restart ClickHouse and confirm that all data is lost (as expected).
+INSERT INTO engine_testing_dec23.memory_table VALUES (1, 'Mikasa', 16), (2, 'eren', 17), (3, 'jinyong', 58), (4, 'daniel park', 18), (5, 'gapryong kim', 58), (6, 'charles choi', 58), (7, 'tom lee', 58), (8, 'luffy', 19), (9, 'levi', 22), (10, 'nami', 23)
 
-Note how Memory engine tables can be used for temporary caching, ETL intermediate steps, or ephemeral testing environments.
+-- Step 3: Preview the table
+SELECT * FROM engine_testing_dec23.memory_table;
 
-Document performance characteristics and limitations.
+SELECT * FROM engine_testing_dec23.memory_table WHERE id < 5;
+
+-- Step 4: Restart the Server and preview the table
+sudo systemctl restart clickhouse-server
+
+clickhouse-client
+
+SELECT * FROM engine_testing_dec23.memory_table;
+
+Query id: a4ea7ed2-82cf-4c08-a1b9-ded9193ff658
+
+Ok.
+
+0 rows in set. Elapsed: 0.002 sec.
